3d9c69793390571a30f5b40aacab1c1123b2bf86
Myer Nore <pnore@hbs.edu>
2017-11-12 21:40:11 -0500

    fix active page not showing in menu

--- src/components/HeaderMenu/HeaderMenu.tsx
+++ src/components/HeaderMenu/HeaderMenu.tsx
@@ -16,13 +16,12 @@ export const HeaderMenu = ({items, pathname, Link, inverted, dispatch}: HeaderMe
   <Container>
     <Menu size="large" pointing secondary inverted={inverted}>
       <Menu.Item as="a" className="mobile only" icon="sidebar" onClick={() => dispatch(toggleSidebar())}/>
-      <Menu.Item as={Link} to="/" name="Gradus Reson Home" className="mobile hidden">
+      <Menu.Item as={Link} to="/" name="Gradus Reason Home" className="mobile hidden">
         <img src={logo} alt="Gradus Reason"/>
       </Menu.Item>
       {items.map((item) => {
-        const withPrefixPath = withPrefix(pathname);
         const prefixedItemPath = withPrefix(item.path);
-        const active = (item.exact) ? withPrefixPath === prefixedItemPath : withPrefixPath.startsWith(prefixedItemPath);
+        const active = (item.exact) ? pathname === prefixedItemPath : pathname.startsWith(prefixedItemPath);
         return <Menu.Item
           as={Link}
           className="mobile hidden"
